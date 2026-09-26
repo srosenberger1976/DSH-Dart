20ff88b4b88c002ff202d254418d2d58fcb93373
srosenberger1976 <s.rosenberger1976@gmail.com>
2026-09-26 17:14:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,14 @@
-const CACHE_NAME = 'app-cache-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'projektname-cache-v2'; // Bei Updates einfach die Version
+erhoehen!
+const ASSETS = [
+'./',
+'./index.html',
+'./manifest.json'
+];
 self.addEventListener('install', (event) => {
 event.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
 });
 self.addEventListener('fetch', (event) => {
-event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
+event.respondWith(caches.match(event.request).then((res) => res ||
+fetch(event.request)));
 });
